d16f16cc750825da268810145888aa44d6b4d6c5
Benicio Lopez <benicio.lopez@gmail.com>
2021-09-28 09:26:25 -0500

    added User model with no hooks yet. Installed jest, sequelize, and bcrypt into the dependencies of the package.json file

--- models/Department.js
+++ models/Department.js
@@ -2,12 +2,19 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('..config/connection');
 
 class Department extends Model {
-    //not sure what is needed here
+    //not sure what is needed here that makes Department different from the Model, will need to research
 }
 
 Department.init({
     id: {
         type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
     }
 },
 {
