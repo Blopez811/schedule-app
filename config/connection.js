98acb5d1b2cdbd79ba1836f68f5b9a418ef217ac
Benicio Lopez <benicio.lopez@gmail.com>
2021-09-28 10:17:14 -0500

    updated the connection.js file to refer to the .env so the mysql username and password are no longer hardcoded and are hidden

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-const sequelize = new Sequelize('scheduler_db', 'root', 'Blopez@811', {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
