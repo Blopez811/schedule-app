df730ee56f5ca9ee1a23a8e8a9c07d62704d3077
Benicio Lopez <benicio.lopez@gmail.com>
2021-09-28 09:38:19 -0500

    added the hooks to the User.js model, should now run bcrypt and hash the user's password before the user is created or updated so the plain text password is not saved on database

--- models/User.js
+++ models/User.js
@@ -38,6 +38,17 @@ User.init(
         }
     },
     {
+        hooks: {
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+
+        },
         sequelize,
         timestamps: false,
         freezeTableName: true,
@@ -45,3 +56,5 @@ User.init(
         modelName: 'user'
     }
 );
+
+module.exports = User;
