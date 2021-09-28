5b08f5053732b46446aaecf7c0b8c959568d47ed
afc2134 <85919290+AFC2134@users.noreply.github.com>
2021-09-28 16:58:12 -0500

    be careful, do not merge

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const routes = require('./routes');
+const sequelize = require('./config/connection');
+const db = require("./models");
+// const exphbs = require('express-handlebars');
+// require("dotenv").config();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(routes);
+
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+  });
+
+
+ module.exports = app;
+
+// const syncOptions = { force: false };
+
+// // If running a test, set syncOptions.force to true
+// // clearing the `testdb`
+// if (process.env.NODE_ENV === "test") {
+//     syncOptions.force = true;
+//   }
+
+
+
+
+
+
+// app.get('/', function (req, res) {
+//     res.send('Home Page')
+//     console.log(`User is admin = ${req.admin}`)
+// })
+
+// app.get('/users', function (req, res) {
+//     res.send('Department Page')
+// })
+
+// function auth(req, res, next) {
+//  if (req.query.admin === 'true') {
+//     req.admin = true 
+//     next()
+// } else {
+//     res.send("not an admin")
+// }
+// }
