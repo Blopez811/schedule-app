8a4e3ef39a316650f8e3daa4824d7262945c4496
Benicio Lopez <benicio.lopez@gmail.com>
2021-09-29 13:54:22 -0500

    heroku deployed server and local host now both work

--- config/connection.js
+++ config/connection.js
@@ -7,12 +7,12 @@ let sequelize;
 if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
   } else {
-      const sequelize = new Sequelize('scheduler_db', 'root', 'Blopez@811', {
-          host: 'localhost',
-          dialect: 'mysql',
-          port: 3306
+      sequelize = new Sequelize('scheduler_db', 'root', 'Blopez@811', {
+         host: 'localhost',
+         dialect: 'mysql',
+         port: 3306
       });
-  }
+};
 
 
 module.exports = sequelize;
