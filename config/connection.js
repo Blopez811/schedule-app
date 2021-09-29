61adc21daab39d9fc2d828c9237922cabe1fd67e
Benicio Lopez <benicio.lopez@gmail.com>
2021-09-29 13:32:11 -0500

    connected to JawsDB

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,18 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-const sequelize = new Sequelize('scheduler_db', 'root', 'Blopez@811', {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-});
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+  } else {
+      const sequelize = new Sequelize('scheduler_db', 'root', 'Blopez@811', {
+          host: 'localhost',
+          dialect: 'mysql',
+          port: 3306
+      });
+  }
+
 
 module.exports = sequelize;
 
