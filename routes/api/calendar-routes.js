469a38befc1e52d4910a5eb45934c89d263b5694
Benicio Lopez <benicio.lopez@gmail.com>
2021-09-30 11:02:18 -0500

    finished get and post routes for calendar

--- routes/api/calendar-routes.js
+++ routes/api/calendar-routes.js
@@ -5,16 +5,31 @@ router.get('/', (req, res) => {
     Calendar.findAll({
         attributes: [
             'id',
-            'event name',
+            'title',
             'date',
             'notes'
         ]
     })
-        .then(dbUserData => res.json(dbUserData))
+        .then(dbCalendarData => res.json(dbCalendarData))
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
         });
+});
+
+router.post('/', (req, res) => {
+    Calendar.create({
+        title: req.body.title,
+        date: req.body.date,
+        notes: req.body.notes
+    })
+    .then(dbCalendarData => {
+        res.json(dbCalendarData)
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
 });
 
 module.exports = router;
