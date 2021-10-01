c50aed4c9590693c07fc6b6ff31d3bc19a6f0948
afc2134 <85919290+AFC2134@users.noreply.github.com>
2021-10-01 16:53:10 -0500

    put route, update dept names

--- routes/api/department-routes.js
+++ routes/api/department-routes.js
@@ -16,34 +16,59 @@ router.post('/', (req, res) => {
     Department.create({
         title: req.body.title
     })
-    .then(dbDepartmentData => {
-        res.json(dbDepartmentData);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    })
+        .then(dbDepartmentData => {
+            res.json(dbDepartmentData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
 });
 
 router.delete('/:id', (req, res) => {
     Department.destroy({
         where: {
-            id : req.params.id
+            id: req.params.id
         }
     })
-    .then(dbDepartmentData => {
-        if (!dbDepartmentData) {
-            res.status(404).json({ message: 'No department found with this id' });
-            return;
-        }
-        res.json(dbDepartmentData);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+        .then(dbDepartmentData => {
+            if (!dbDepartmentData) {
+                res.status(404).json({ message: 'No department found with this id' });
+                return;
+            }
+            res.json(dbDepartmentData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 })
 
+router.put('/:id', (req, res) => {
+    Department.update(
+        {
+            title: req.body.title
+        },
+        {
+            where: {
+
+                id: req.params.id
+
+            }
+        }
+    )
+        .then(dbDepartmentData => {
+            if (!dbDepartmentData) {
+                res.status(404).json({ message: 'No department found with this id' });
+                return;
+            }
+            res.json(dbDepartmentData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+})
 
 
 
