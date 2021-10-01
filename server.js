492a70736c37be09d7c1c9862963083ea3340ad6
Benicio Lopez <benicio.lopez@gmail.com>
2021-10-01 10:17:37 -0500

    force resetting server

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(routes);
 
 
 
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
   });
 
