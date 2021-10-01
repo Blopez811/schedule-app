5c56c0f9c5a8bc13fa05f72d599fd8e423389f0f
Benicio Lopez <benicio.lopez@gmail.com>
2021-10-01 10:25:03 -0500

    changed force back to false

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(routes);
 
 
 
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
   });
 
