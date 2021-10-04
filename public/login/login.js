732c332be0fc7a7c7dfed46b3792af6e03d9ac8c
Benicio Lopez <benicio.lopez@gmail.com>
2021-10-04 10:31:21 -0500

    the user name  and the password inputted by the user  are now being captured and console logged by the loginHandler function

--- public/login/login.js
+++ public/login/login.js
@@ -2,7 +2,13 @@ const loginBtn = document.querySelector('#loginBtn');
 
 function loginHandler() {
     event.preventDefault();
- console.log('loginHandler function fired!');
+
+    const username = document.querySelector('#floatingInput').value.trim();
+    const password = document.querySelector('#floatingPassword').value.trim();
+    console.log('loginHandler function fired!');
+    console.log(username);
+    console.log(password);
+
  
 };
 
