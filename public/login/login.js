3fa8b2bfafe19c326bb69b2f427d6ccf467a7b20
Benicio Lopez <benicio.lopez@gmail.com>
2021-10-04 10:04:54 -0500

    fixed the relative paths for the css/images on login page and changed them to absolute paths so they now are recognized by handlebars and functioning

---
+++ public/login/login.js
@@ -0,0 +1,9 @@
+const loginBtn = document.querySelector('#loginBtn');
+
+function loginHandler() {
+ console.log('loginHandler function fired!');
+ 
+};
+
+
+loginBtn.addEventListener('click', loginHandler);
